9d053a297a42765c2a5b0214d16bf0a3ecd1c023
Richavelly <zigafree13@gmail.com>
2022-06-18 01:02:09 +0300

    #4-Task: 'Fighter card'

---
+++ DZ_2/CardFighter.js
@@ -0,0 +1,34 @@
+const myFighter = {
+  name: 'Franko "Axe"',
+  win: 16,
+  loose: 3,
+  draw: 5,
+  age: 40,
+  division: 'heavyweight',
+  rivals: [ 'Richard "Adamant"', 'Lewis "Bullet"', 'Freddy "Krueger"' ]
+};
+
+// [{ key: 'name', value: 'Franko"Axe"'}, {...}]
+const fighterArray = [];
+
+// prop - ключ на данном шаге цикла
+// myFighter - объект
+for (let prop in myFighter) {
+    const element = {
+        key: prop,
+        value: myFighter[prop]
+    }
+    fighterArray.push(element);
+}
+console.log(fighterArray);
+
+const cardFighter = {};
+for (let nameAge of fighterArray) {
+    if (nameAge.key === 'name') {
+        cardFighter.name = nameAge.value;
+    }
+    if (nameAge.key === 'age') {
+        cardFighter.age = nameAge.value;
+    }
+}
+console.log(cardFighter);

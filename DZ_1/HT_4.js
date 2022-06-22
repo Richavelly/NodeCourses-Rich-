c6511e711fdbd226d011c3e4aaaa961c4a6fe9de
Richavelly <zigafree13@gmail.com>
2022-06-22 19:51:38 +0300

    Fourth task

--- DZ_1/HT_4.js
+++ DZ_1/HT_4.js
@@ -1,11 +1,15 @@
-let originalNumber = 10;
-let degreeNumber = "fmlgml";
+let originalNumber = "10";
+let degreeNumber = '2';
 let resultNumber;
 
-if (typeof originalNumber === "number" && typeof degreeNumber === "number") {
+if ((typeof originalNumber === "number" || typeof originalNumber === "string") && (typeof degreeNumber === "number" || typeof degreeNumber === "string")) {
     resultNumber = originalNumber ** degreeNumber;
-    console.log("Резултат: ", resultNumber);
+    
+    if (Number.isNaN(resultNumber)) {
+        console.log("ОШИБКА!\nНедопустимый тип файла!");
+    } else {
+        console.log("Результат: ", resultNumber);
+    }
 } else {
     console.log("ОШИБКА!\nНедопустимый тип файла!");
 }
-
